e72429a52719709c9ede4d1c688ffc0a16a60d3f
deadbeef <deadbeef@localhost>
2026-07-09 18:34:59 +0200

    FS-670-HDS-010-SDS-010-SMS-040: select active lab

--- current-lab/metadata.nix
+++ current-lab/metadata.nix
@@ -1,9 +1,9 @@
 {
   layer = "SMT";
-  selector = "FS-840-HDS-010-SDS-010-SMS-040";
-  traceId = "FS-840-HDS-010-SDS-010-SMS-040";
-  sourceKind = "intent-source";
-  sourceRoot = "GAMP/SMT/FS-840-HDS-010-SDS-010-SMS-040";
-  sourcePath = "GAMP/SMT/FS-840-HDS-010-SDS-010-SMS-040/intent.nix";
-  selectedBy = "scripts/select-current-lab.sh SMT FS-840-HDS-010-SDS-010-SMS-040";
+  selector = "FS-670-HDS-010-SDS-010-SMS-040";
+  traceId = "FS-670-HDS-010-SDS-010-SMS-040";
+  sourceKind = "construction-only";
+  sourceRoot = "GAMP/SMT/FS-670-HDS-010-SDS-010-SMS-040";
+  sourcePath = "GAMP/SMT/FS-670-HDS-010-SDS-010-SMS-040/default.nix";
+  selectedBy = "scripts/select-current-lab.sh SMT FS-670-HDS-010-SDS-010-SMS-040";
 }
