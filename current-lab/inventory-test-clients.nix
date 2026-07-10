13c013b85fe7e2b4d170d1ef98c14a008eee2ae8
deadbeef <deadbeef@localhost>
2026-07-10 10:06:19 +0200

    FS-630-HDS-010-SDS-010-SMS-020: select active lab

--- current-lab/inventory-test-clients.nix
+++ current-lab/inventory-test-clients.nix
@@ -1,5 +1,5 @@
 let
-  source = import ../GAMP/SMT/FS-940-HDS-010-SDS-020-SMS-050/inventory-test-clients.nix;
+  source = import ../GAMP/SMT/FS-630-HDS-010-SDS-010-SMS-020/inventory-test-clients.nix;
   managementVlan2 = {
     bridge = "vlan2";
     ipv4 = {
--- current-lab/metadata.nix
+++ current-lab/metadata.nix
@@ -1,9 +1,9 @@
 {
   layer = "SMT";
-  selector = "FS-940-HDS-010-SDS-020-SMS-050";
-  traceId = "FS-940-HDS-010-SDS-020-SMS-050";
+  selector = "FS-630-HDS-010-SDS-010-SMS-020";
+  traceId = "FS-630-HDS-010-SDS-010-SMS-020";
   sourceKind = "intent-source";
-  sourceRoot = "GAMP/SMT/FS-940-HDS-010-SDS-020-SMS-050";
-  sourcePath = "GAMP/SMT/FS-940-HDS-010-SDS-020-SMS-050/intent.nix";
-  selectedBy = "scripts/select-current-lab.sh SMT FS-940-HDS-010-SDS-020-SMS-050";
+  sourceRoot = "GAMP/SMT/FS-630-HDS-010-SDS-010-SMS-020";
+  sourcePath = "GAMP/SMT/FS-630-HDS-010-SDS-010-SMS-020/intent.nix";
+  selectedBy = "scripts/select-current-lab.sh SMT FS-630-HDS-010-SDS-010-SMS-020";
 }
