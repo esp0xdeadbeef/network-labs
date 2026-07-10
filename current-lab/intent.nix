db9b6c84699a637969e9a4ec25f7053818eb8aad
deadbeef <deadbeef@localhost>
2026-07-10 03:09:46 +0200

    FS-740-HDS-040-SDS-010-SMS-010: select active lab

--- current-lab/intent.nix
+++ current-lab/intent.nix
@@ -1,1 +1,1 @@
-import ../GAMP/SMT/FS-660-HDS-010-SDS-010-SMS-010/intent.nix
+import ../GAMP/SMT/FS-740-HDS-040-SDS-010-SMS-010/intent.nix
--- current-lab/inventory-test-clients.nix
+++ current-lab/inventory-test-clients.nix
@@ -1,5 +1,5 @@
 let
-  source = import ../GAMP/SMT/FS-660-HDS-010-SDS-010-SMS-010/inventory-test-clients.nix;
+  source = import ../GAMP/SMT/FS-740-HDS-040-SDS-010-SMS-010/inventory-test-clients.nix;
   managementVlan2 = {
     bridge = "vlan2";
     ipv4 = {
