da438d487996e00ae7ccae244283131b3fe60521
deadbeef <deadbeef@localhost>
2026-07-10 03:52:03 +0200

    FS-960-HDS-010-SDS-010-SMS-070: select active lab

--- current-lab/intent-s-router-test-clients.nix
+++ current-lab/intent-s-router-test-clients.nix
@@ -5,7 +5,7 @@ in
 rec {
   control_plane_model = {
     meta = {
-      traceId = "FS-760-HDS-020-SDS-010-SMS-010";
+      traceId = "FS-960-HDS-010-SDS-010-SMS-070";
       source = "network-labs current-lab SMT/SIT client-host no-endpoint source";
     };
     deployment.hosts.s-router-test-clients = testClientHost // {
--- current-lab/metadata.nix
+++ current-lab/metadata.nix
@@ -1,9 +1,9 @@
 {
   layer = "SMT";
-  selector = "FS-760-HDS-020-SDS-010-SMS-010";
-  traceId = "FS-760-HDS-020-SDS-010-SMS-010";
+  selector = "FS-960-HDS-010-SDS-010-SMS-070";
+  traceId = "FS-960-HDS-010-SDS-010-SMS-070";
   sourceKind = "intent-source";
-  sourceRoot = "GAMP/SMT/FS-760-HDS-020-SDS-010-SMS-010";
-  sourcePath = "GAMP/SMT/FS-760-HDS-020-SDS-010-SMS-010/intent.nix";
-  selectedBy = "scripts/select-current-lab.sh SMT FS-760-HDS-020-SDS-010-SMS-010";
+  sourceRoot = "GAMP/SMT/FS-960-HDS-010-SDS-010-SMS-070";
+  sourcePath = "GAMP/SMT/FS-960-HDS-010-SDS-010-SMS-070/intent.nix";
+  selectedBy = "scripts/select-current-lab.sh SMT FS-960-HDS-010-SDS-010-SMS-070";
 }
