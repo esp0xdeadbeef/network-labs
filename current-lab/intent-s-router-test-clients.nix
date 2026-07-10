0892579967f890eaeaf77d1d91674b3619e023d3
deadbeef <deadbeef@localhost>
2026-07-10 08:35:08 +0200

    FS-820-HDS-010-SDS-010-SMS-030: select active lab

--- current-lab/intent-s-router-test-clients.nix
+++ current-lab/intent-s-router-test-clients.nix
@@ -5,7 +5,7 @@ in
 rec {
   control_plane_model = {
     meta = {
-      traceId = "FS-940-HDS-010-SDS-020-SMS-020";
+      traceId = "FS-820-HDS-010-SDS-010-SMS-030";
       source = "network-labs current-lab SMT/SIT client-host no-endpoint source";
     };
     deployment.hosts.s-router-test-clients = testClientHost // {
