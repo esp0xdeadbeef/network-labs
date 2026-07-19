3639443eabf8cb27c5e3f3343ed5763b48ec4d2f
deadbeef <deadbeef@localhost>
2026-07-19 05:59:58 +0200

    active-lab: select FS-540 reproducible DNS restage

--- current-lab/metadata.nix
+++ current-lab/metadata.nix
@@ -1,9 +1,9 @@
 {
   layer = "SMT";
-  selector = "FS-270-HDS-010-SDS-010-SMS-020";
-  traceId = "FS-270-HDS-010-SDS-010-SMS-020";
+  selector = "FS-540-HDS-010-SDS-010-SMS-045";
+  traceId = "FS-540-HDS-010-SDS-010-SMS-045";
   sourceKind = "intent-source";
-  sourceRoot = "GAMP/SMT/FS-270-HDS-010-SDS-010-SMS-020";
-  sourcePath = "GAMP/SMT/FS-270-HDS-010-SDS-010-SMS-020/intent.nix";
-  selectedBy = "scripts/select-current-lab.sh SMT FS-270-HDS-010-SDS-010-SMS-020";
+  sourceRoot = "GAMP/SMT/FS-540-HDS-010-SDS-010-SMS-045";
+  sourcePath = "GAMP/SMT/FS-540-HDS-010-SDS-010-SMS-045/intent.nix";
+  selectedBy = "scripts/select-current-lab.sh SMT FS-540-HDS-010-SDS-010-SMS-045";
 }
